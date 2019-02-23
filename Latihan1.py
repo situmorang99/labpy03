8e9b233a9462549a8df04db13bb46c83692d62ed
situmorang99 <lestina.s10@mhs.pelitabangsa.ac.id>
2019-02-23 23:22:21 +0700

    labpy03

---
+++ Latihan1.py
@@ -0,0 +1,10 @@
+import random
+jumlah = int(input("Masukan Nilai N :"))
+for i in range(jumlah):
+    i=random.uniform(0.0,0.5)
+    print("Data ke : 1 =>",i)
+
+print("*******************")
+print("Selesai")
+print("*******************")
+
---
+++ Latihan2.py
@@ -0,0 +1,10 @@
+print("\nMenentukan Bilangan Terbesar")
+print("\n")
+max=0
+while True:
+    a=int(input("Masukan Bilangan :"))
+    if max < a:
+        max = a
+    if a==0:
+        break
+print("Bilangan Terbesar = ", max)
